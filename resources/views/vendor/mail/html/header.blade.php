736521bb1798c1b08a4571677041e93cbb391251
Adi Fayyaz <adisumardi888@gmail.com>
2026-04-24 08:51:03 +0700

    fix: perbesar logo email 48px → 96px, pakai logo-fix.png

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="https://tawzii.id/logo.png" class="logo" alt="Tawzii Digital" style="height:48px;width:48px;border-radius:50%;object-fit:cover;">
+<img src="https://tawzii.id/logo-fix.png" class="logo" alt="Tawzii Digital" style="height:96px;width:96px;border-radius:50%;object-fit:cover;">
 </a>
 </td>
 </tr>
